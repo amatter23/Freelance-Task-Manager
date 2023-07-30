cc940c4e00d25e2d1f945f044c9dd683856165e6
Ahmed Matter <matterahmed36@gmail.com>
2023-07-30 05:58:58 +0300

    add home page

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,12 +2,25 @@
 import React from 'react';
 // import css file
 import classes from './Home.module.css';
+// import fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+// import task component
+import Task from '../../components/Task/Task';
 // react component
 function Home() {
   return (
-    <div className={classes.Home}>
-      <h1>Home</h1>
-      <p>Home page</p>
+    <div className={classes.home}>
+      {/* <div className={classes.left}>
+        <h5>Last 3 days tasks</h5>
+      </div>
+      <div className={classes.right}>
+        <div className={classes.top}></div>
+        <div className={classes.bottom}>
+          <FontAwesomeIcon icon={faHouse} />
+        </div>
+      </div> */}
+      <Task />
     </div>
   );
 }
