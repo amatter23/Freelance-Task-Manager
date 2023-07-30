de353f89f0b9b564470bf6477b58d604a60d69f1
Ahmed Matter <matterahmed36@gmail.com>
2023-07-30 05:59:28 +0300

    task task page to route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Tasks from './Pages/Tasks/Tasks';
+import Route from '../src/routes/Route';
 function App() {
   const userRoute = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: <Route />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/tasks', element: <Tasks /> },
+      ],
     },
   ]);
   return <RouterProvider router={userRoute} />;
