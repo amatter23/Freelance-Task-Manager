3d360a8c8ec6e86ff5c5369231fcb06fb1a5f0d5
Ahmed Matter <matterahmed36@gmail.com>
2023-07-31 18:25:07 +0300

    update a app route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Pages/Home/Home';
+import Home from '../src/Pages/Home/Home';
 import Tasks from './Pages/Tasks/Tasks';
 import Route from '../src/routes/Route';
+import AddTask from './components/Task/AddTask';
 function App() {
+  // todo create a home page
   const userRoute = createBrowserRouter([
     {
       path: '/',
       element: <Route />,
       children: [
-        { path: '/', element: <Home /> },
+        { path: '/', element: <Tasks /> },
         { path: '/tasks', element: <Tasks /> },
       ],
     },
