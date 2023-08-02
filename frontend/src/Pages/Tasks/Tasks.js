9631b5b515c1bc1503c8735d34efa368deeb1d2d
Ahmed Matter <matterahmed36@gmail.com>
2023-08-02 08:53:38 +0300

    reorganization the code

--- frontend/src/Pages/Tasks/Tasks.js
+++ frontend/src/Pages/Tasks/Tasks.js
@@ -84,20 +84,7 @@ const Tasks = props => {
   const addCustomer = data => {
     addCustomerMutation.mutate(data);
   };
-  if (tasks.isLoading || customers.isLoading)
-    return (
-      <div className={classes.empty}>
-        <FontAwesomeIcon icon={faSpinner} spin={true} size='2xl' />
-      </div>
-    );
-  if (tasks.isError || customers.isError)
-    return (
-      <div className={classes.empty}>
-        <FontAwesomeIcon color='red' size='2xl' icon={faCircleXmark} />{' '}
-        <h4>Something went wrong please try again</h4>
-      </div>
-    );
-
+  // filter tasks
   const filteredTasks = tasks.data.filter(task => {
     if (!customer && !date && !from && !to) {
       // Return true for all tasks when both customer and date are null
@@ -128,8 +115,23 @@ const Tasks = props => {
       );
     }
   });
-
+  // check if tasks are empty
   const areTasksEmpty = filteredTasks.length === 0;
+
+// loading and error states
+  if (tasks.isLoading || customers.isLoading)
+    return (
+      <div className={classes.empty}>
+        <FontAwesomeIcon icon={faSpinner} spin={true} size='2xl' />
+      </div>
+    );
+  if (tasks.isError || customers.isError)
+    return (
+      <div className={classes.empty}>
+        <FontAwesomeIcon color='red' size='2xl' icon={faCircleXmark} />{' '}
+        <h4>Something went wrong please try again</h4>
+      </div>
+    );
 
   return (
     <div className={classes.container}>
